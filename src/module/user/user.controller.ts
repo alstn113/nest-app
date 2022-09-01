652479f8abe9053d75e956cd7808194f799f2cf5
neko113 <alstn113@gmail.com>
2022-09-01 12:22:22 +0900

    refactor : user.controller 수정

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,29 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GetCurrentUserId } from 'src/common/decorators/get-current-user-id.decorator';
-import { Public } from 'src/common/decorators/public.decorator';
 import { UserResponseDto } from './dto/user-response';
 import { UserService } from './user.service';
-import { Request } from 'express';
 
 @Controller('/user')
 @ApiTags('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  /** 
-    TODO: null은 여기서 처리할게 아니라 client에서 처리해야함
-    다 지우고 return req.user만 있으면 될 듯
-    client에서 try catch해서 error면 null 반환이 맞는 듯. 
-  */
-  @Public()
   @Get('/me')
   async getCurrentUser(
     @GetCurrentUserId() userId: string,
-    req: Request,
   ): Promise<UserResponseDto> {
-    console.log(req.userId);
-    if (userId === undefined) return null;
     return await this.userService.findUserById(userId);
   }
 }
