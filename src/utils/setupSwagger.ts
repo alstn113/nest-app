49cb6b8731b76a7e1e52c73795befe375978df86
neko113 <alstn113@gmail.com>
2022-09-01 13:19:44 +0900

    feat : swagger setBasePath '/api'

--- src/utils/setupSwagger.ts
+++ src/utils/setupSwagger.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 export function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
     .setTitle('NEXT')
+    .setBasePath('api')
     .setVersion('1.0.0')
     .build();
 
