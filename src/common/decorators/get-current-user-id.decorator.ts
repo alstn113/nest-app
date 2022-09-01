1f09f6198f6a99afe5215889db54d976157408e8
neko113 <alstn113@gmail.com>
2022-09-01 22:56:31 +0900

    refactor : GetCurrentUserId decorator type 수정

--- src/common/decorators/get-current-user-id.decorator.ts
+++ src/common/decorators/get-current-user-id.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetCurrentUserId = createParamDecorator(
-  (_data: unknown, context: ExecutionContext): number => {
+  (_data: unknown, context: ExecutionContext): number | null => {
     const req = context.switchToHttp().getRequest();
     return req.user?.id;
   },
